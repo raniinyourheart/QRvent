0a1e1140428c02a1af2c5f85dda5d99e343cec52
Rani Maharani <maharanilintang83@gmail.com>
2026-08-03 21:29:32 +0700

    layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,12 @@
 "use client";
 
+import { useEffect, useState } from "react";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
 
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
