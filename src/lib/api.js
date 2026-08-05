280327058208278e0d55e8fdc33f7de4a2e5e709
Rani Maharani <maharanilintang83@gmail.com>
2026-08-05 22:48:08 +0700

    api

--- src/lib/api.js
+++ src/lib/api.js
@@ -9,7 +9,19 @@ const api = axios.create({
   },
 });
 
-
+// Interceptor untuk menambahkan token ke setiap request
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 // Interceptor untuk handle response error (token expired)
 api.interceptors.response.use(
